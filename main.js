fb5d16b42e244d79f8a6ec035a3384bf53b77a0f
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-19 15:57:11 -0500

    Edit Files with linters

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-class books{
-    constructor(tittle,autor){
-        this.tittle = tittle;
-        this.autor= autor;
-    }
+/* eslint-disable max-classes-per-file */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable new-cap */
+
+const form = document.getElementById('books');
+
+class books {
+  constructor(tittle, autor) {
+    this.tittle = tittle;
+    this.autor = autor;
+  }
 }
 
-class UI{
-    addbook(book){
-        const productCard = document.getElementById('statusbooks');
-        const element = document.createElement('div');
-        element.innerHTML =`
+class UI {
+  addbook(book) {
+    const productCard = document.getElementById('statusbooks');
+    const element = document.createElement('div');
+    element.innerHTML = `
         <div>
             <div class="bookstyle">
                 ${book.tittle}<br>
@@ -19,55 +25,47 @@ class UI{
             </div>
         </div>
         `;
-        productCard.appendChild(element);
-        this.resetform();
+    productCard.appendChild(element);
+    this.resetform();
+  }
+
+  resetform() {
+    document.getElementById('books').reset();
+  }
+
+  removebook(element) {
+    if (element.name === 'deletebook') {
+      element.parentElement.parentElement.parentElement.remove();
     }
-    resetform(){
-        document.getElementById('books').reset();
-    }
-    removebook(element){
-        if(element.name ==='deletebook'){
-            element.parentElement.parentElement.parentElement.remove();
-        }
-    }
+  }
 }
 
-const form = document.getElementById('books');
-form.addEventListener("submit", send);
+function send(event) {
+  const tittlebook = document.getElementById('tittleBook').value;
+  const name = document.getElementById('nameAutor').value;
+  const book = new books(tittlebook, name);
+  const ui = new UI();
+  ui.addbook(book);
 
+  /* local storage */
+  if (localStorage.getItem('books') === null) {
+    const books = [];
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  } else {
+    const books = JSON.parse(localStorage.getItem('books'));
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
+  event.preventDefault();
+}
 
-function send(event){
-    const tittlebook = document.getElementById('tittleBook').value;
-    const name = document.getElementById('nameAutor').value;
-    const book = new books(tittlebook,name);
-    const ui = new UI;
-    ui.addbook(book);
+form.addEventListener('submit', send);
 
-    /* local storage */
-
-    if(localStorage.getItem('books')=== null){
-        let books = [];
-        books.push(book);
-        localStorage.setItem('books', JSON.stringify(books));
-    }else{
-        let books = JSON.parse(localStorage.getItem('books'));
-        books.push(book);
-        localStorage.setItem('books', JSON.stringify(books));
-    }
-
-    event.preventDefault();
-
+function erasebook(e) {
+  const ui = new UI();
+  ui.removebook(e.target);
 }
 
 document.getElementById('statusbooks').addEventListener('click', erasebook);
-
-function erasebook(e){
-    const ui = new UI();
-    ui.removebook(e.target);
-}
-
-
-
-
-
